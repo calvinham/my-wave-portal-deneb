fd6a2e432e706c9039fd13145ef942685cecb89d
calvinham <calvinjunham@gmail.com>
2021-10-03 15:14:01 -0700

    add files

---
+++ scripts/deploy.js
@@ -0,0 +1,28 @@
+const main = async () => {
+	const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
+	const waveContract = await waveContractFactory.deploy({
+		value: hre.ethers.utils.parseEther('0.001'),
+	});
+
+	await waveContract.deployed();
+
+	console.log('WavePortal address: ', waveContract.address);
+};
+
+const runMain = async () => {
+	try {
+		await main();
+		process.exit(0);
+	} catch (error) {
+		console.error(error);
+		process.exit(1);
+	}
+};
+
+runMain();
+
+// npx hardhat node
+// live ethereum test network
+
+// npx hardhat run scripts/deploy.js --network localhost
+//
